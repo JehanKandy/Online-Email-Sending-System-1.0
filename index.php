54c0d42d5593b77d57d9ae74d20817d492615601
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-05-09 23:00:59 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Email Sending System</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<nav class="navbar navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">&nbsp;&nbsp;Email Sending System</a>
+    <span class="navbar-text">
+        Send Emails &nbsp&nbsp&nbsp&nbsp
+        <a href="login/login.php"><button class="btn btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z"/>
+  <path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+</svg> &nbsp; Login</button></a>&nbsp;&nbsp;&nbsp;&nbsp;
+    </span>
+</nav>
+
+<div class="h-contant">
+    <br>
+    <h1>Email Sending System</h1>
+    <table border="0">
+        <tr>
+            <td>               
+                <img src="images/email1.png" alt="email1" class="img1">
+            </td>
+            <td>
+                &nbsp;&nbsp;
+            </td>
+            <td>               
+                <img src="images/email2.jpg" alt="email1" class="img1">                
+            </td>
+        </tr>
+        <tr>
+            <td colspan="3"><p></p></td>
+        </tr>
+        <tr>
+            <td>               
+                <img src="images/email3.jpg" alt="email1" class="img1">
+            </td>
+            <td>
+                &nbsp;&nbsp;
+            </td>
+            <td>               
+                <img src="images/email4.jpg" alt="email1" class="img1">                
+            </td>
+        </tr>
+
+        
+    </table>
+    <br><br>
+</div>
+
+<div class="footer">
+    <div class="card text-center">
+    <div class="card-footer text-muted">
+        &copy; &nbspDEVELOPED BY : <a href="https://github.com/JehanKandy" target="_blank" style="text-decoration: none;">JEHANKANDY</a> || 2022 May
+    </div>
+    </div>
+</div>
+
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ login/login.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+<nav class="navbar navbar-dark bg-dark">
+    <a class="navbar-brand" href="../index.php">&nbsp;&nbsp;Email Sending System - login</a>
+    <span class="navbar-text">
+        Send Emails &nbsp&nbsp&nbsp&nbsp
+        <a href="../index.php"><button class="btn btn-primary"> &nbsp; back</button></a>&nbsp;&nbsp;&nbsp;&nbsp;
+    </span>
+</nav>
+
+<div class="login-content">
+    <br><br><br><br><br><br>
+        <div class="login-box">
+            <h1>login</h1>
+            <hr style="color: grey;">
+            <br>
+                <form action="<?php echo($_SERVER['PHP_SELF']); ?>" method="POST">
+                    <p>Username : </p>
+                    <input type="text" name="username" placeholder="Username" class="login-input">
+                    <br><br>
+                    <p>Password : </p>
+                    <input type="password" name="password" placeholder="Password" class="login-input">
+                    <br><br>
+                    <input type="submit" value="Login" name="login" class="login-btn">
+                </form>
+            
+            <hr style="color: grey;">
+            <p>Don't have an Account ? <a href="reg.php" style="text-decoration: none; color:#2949b4;">Create One</a></p>            
+        </div>
+    <br><br><br><br><br><br><br><br>
+</div>
+
+
+
+<div class="footer">
+    <div class="card text-center">
+    <div class="card-footer text-muted">
+        &copy; &nbspDEVELOPED BY : <a href="https://github.com/JehanKandy" target="_blank" style="text-decoration: none;">JEHANKANDY</a> || 2022 May
+    </div>
+    </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
